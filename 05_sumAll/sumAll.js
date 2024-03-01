b96d98203577b98917b4856e4197bc36ed36bbc5
sean-nl <sean.n.lamb.arch@gmail.com>
2024-03-01 18:31:54 -0500

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end)
+        || start < 0 || end < 0) {
+        return 'ERROR';
+    }
 
+    // swap using array destructuring syntax
+    if (start > end) [start, end] = [end, start];
+
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
