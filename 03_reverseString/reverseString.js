3f2b184c5d4ebfbacf9cb64f731c03edd6cff521
sean-nl <sean.n.lamb.arch@gmail.com>
2024-02-29 22:38:23 -0500

    Exercises 1-3 complete. Exercise 04: working for single and multiple values (tests 1 and 2)

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(phrase) {
+    let reversedPhrase = '';
 
+    for (let i = phrase.length - 1; i >= 0; i--) {
+        reversedPhrase += phrase[i]
+    }
+    return reversedPhrase;
 };
 
 // Do not edit below this line
