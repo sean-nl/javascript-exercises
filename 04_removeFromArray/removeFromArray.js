854012b4913157f441a6eeb5e55739f79ec6d66c
sean-nl <sean.n.lamb.arch@gmail.com>
2024-02-29 22:41:16 -0500

    Remove redundant code in Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,27 +1,5 @@
 const removeFromArray = function(arrayIn, ...values) {
-    let removedArray = [];
-
-
-    //old method
-    // for (const value of values) {
-    //     const index = arrayIn.indexOf(value);
-    //     if (index > -1) {
-    //         let a = arrayIn.slice(0,index); //Naming these for debug
-    //         let ra = removedArray.concat(arrayIn.slice(0,index)); //Naming these for debug
-
-    //         //slice the original array up until the occurence of the value, and concat it to the removedArray.
-    //         removedArray = removedArray.concat(arrayIn.slice(0,index)); //Is there a way of doing this without concat?
-            
-    //         //remove that chunk of the original array.
-    //         arrayIn = arrayIn.slice(index+1);
-    //     }
-    // }
-
-    // return removedArray.concat(arrayIn);
-
-    //   new method
     for (const value of values) {
-
         //get one index
         const index = arrayIn.indexOf(value);
 
@@ -30,7 +8,7 @@ const removeFromArray = function(arrayIn, ...values) {
         }
     }
 
-    return removedArray.concat(arrayIn); //Append the remaining piece of arrayIn
+    return arrayIn;
 };
 
 // Do not edit below this line
